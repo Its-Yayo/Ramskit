7ba1cd7098cbf6d44b4e18bd626ee4ad7edcd3b2
Its-Yayo <a01754574@itesm.mx>
2023-07-13 10:17:42 -0600

    Update Fix uses and improvements

--- src/ramskit.py
+++ src/ramskit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-""" Copyright (C) 2023 Its-Yayo
+""" 
+Copyright (C) 2023 Its-Yayo
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
