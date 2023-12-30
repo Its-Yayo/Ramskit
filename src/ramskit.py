738e3518cc8156f72b6affbc3b8c92529b637a86
Its-Yayo <a01754574@itesm.mx>
2023-12-29 19:17:27 -0600

    update code text

--- src/ramskit.py
+++ src/ramskit.py
@@ -16,8 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>. """
 
 
-import os.path
 from cryptography.fernet import Fernet
+import os.path
+
 
 CURRENT = "v1.1.0"
 
