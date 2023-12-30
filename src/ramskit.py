ea8915be5e4f84bd32f9ace30b490c0839f625b4
Its-Yayo <a01754574@itesm.mx>
2023-12-30 12:28:43 -0600

    main.py: update TODOs #2

--- src/ramskit.py
+++ src/ramskit.py
@@ -61,7 +61,6 @@ class Ramskit:
     def decrypt_file(self, items, key) -> None:
         fernet = Fernet(key)
 
-        # TODO: Add try statement for file decryption
         for item in items:
             normalized_item = os.path.normpath(item)
 
