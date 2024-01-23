9993bccf75cd302db54583de138ae98baf78413c
Yayo <88682615+Its-Yayo@users.noreply.github.com>
2024-01-22 22:04:27 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ import sys
 import argparse
 
 
-# TODO: .deb and .exe package for v2.0.0 -> Means for apt package too!
+# TODO: .deb and .exe package for v2.0.0 -> Means for apt package too for Debian-based distros!
 def main() -> None:
     parser = argparse.ArgumentParser(description="Ramskit - CLI Usages")
     parser.add_argument('-a', '--action', dest="action", required=True,
