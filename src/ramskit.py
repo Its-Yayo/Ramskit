b428d35c5cd78d15ac31c4416d08992d35c446e2
Yayo <88682615+Its-Yayo@users.noreply.github.com>
2024-01-23 23:02:24 -0600

    debug delete

--- src/ramskit.py
+++ src/ramskit.py
@@ -38,7 +38,7 @@ class Ramskit:
             with open('key.key', "wb") as f:
                 self.key = Ramskit.generate_key()
 
-        print(f"[Debug] Key is {self.key}")
+        # print(f"[Debug] Key is {self.key}")
 
     def encrypt_file(self, items: collections.Iterable, key: bytes) -> None:
         fernet = Fernet(key)
