99227140ca43e08d4b9c2ba21bd3ed257805aa8a
Yayo <88682615+Its-Yayo@users.noreply.github.com>
2024-02-07 00:17:54 -0600

    delete TODO init

--- src/ramskit.py
+++ src/ramskit.py
@@ -23,7 +23,6 @@ import os.path
 CURRENT = "v1.2.0"
 
 
-# TODO: Update CLI addition time for encrypt/decrypt for v2.0.1
 class Ramskit:
     def __init__(self) -> None:
         print(f"[x] Starting Ramskit {CURRENT}")
