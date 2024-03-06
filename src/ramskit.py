b8ab0309b08060061744bae94a625939f3687964
Yayo <88682615+Its-Yayo@users.noreply.github.com>
2024-03-06 15:14:12 -0600

    docstrings check

--- src/ramskit.py
+++ src/ramskit.py
@@ -43,7 +43,7 @@ class Ramskit:
 
     def encrypt_file(self, items: collections.Iterable, key: bytes) -> None:
         """ Encrypter's logic. It checks the key assigned in order to overwrite the file.
-        It will generate a .txt file for some additional instructions to decrypt the data."""
+        It will generate a .txt file for some additional instructions to decrypt the data. """
         fernet = Fernet(key)
 
         for item in items:
@@ -65,7 +65,7 @@ class Ramskit:
                            f"Use the '-a or --action decrypt' usage to decrypt this file!")
 
     def decrypt_file(self, items: collections.Iterable, key: bytes) -> None:
-        """Decrypter's logic. It will decrypt the data, as well as deleting the .txt file."""
+        """Decrypter's logic. It will decrypt the data, as well as deleting the .txt file.  """
         fernet = Fernet(key)
 
         for item in items:
@@ -87,7 +87,7 @@ class Ramskit:
 
     @staticmethod
     def generate_key() -> int:
-        """Method that will generate a 2-bit url-safe base64-encoded key for CLI Usages"""
+        """ Method that will generate a 2-bit url-safe base64-encoded key for CLI Usages. """
         key = Fernet.generate_key()
 
         with open('key.key', 'wb') as file:
@@ -95,7 +95,7 @@ class Ramskit:
 
     @staticmethod
     def load_key() -> bytes:
-        """Method that will load the key in order to overwrite the init key file"""
+        """ Method that will load the key in order to overwrite the init key file. """
         with open('key.key', 'rb') as file:
             return file.read()
 
