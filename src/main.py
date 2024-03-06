8d8959cb3a08a21cdfbcd4d33673ed83802dca5f
Yayo <88682615+Its-Yayo@users.noreply.github.com>
2024-03-06 15:17:04 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-""" Copyright (C) 2023-2024 Luis De León
+""" Copyright (C) Luis De León
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
